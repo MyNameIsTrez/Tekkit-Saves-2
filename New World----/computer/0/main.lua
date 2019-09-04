d3304ba892cbbdae14030f10bfe41e11c122d7c7
MyNameIsTrez <32989873+MyNameIsTrez@users.noreply.github.com>
2019-09-04 13:22:51 +0200

    Added scrollAllChars, but it's currently only displaying random chars on the left segment.

--- New World----/computer/0/main.lua
+++ New World----/computer/0/main.lua
@@ -1,29 +1,68 @@
 -- Made by MyNameIsTrez in 2019
 bundledOutputSide = 'back'
 
-local binary = {}
-binary['0'] = 63
-binary['1'] = 6
-binary['2'] = 91
-binary['3'] = 79
-binary['4'] = 102
-binary['5'] = 109
-binary['6'] = 125
-binary['7'] = 7
-binary['8'] = 127
-binary['9'] = 111
-binary['a'] = 119
-binary['b'] = 124
-binary['c'] = 57
-binary['d'] = 94
-binary['e'] = 121
-binary['f'] = 113
+-- local numbers = '0123456789'
+-- local alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-function binaryContains(key)
-  return binary[key] ~= nil
+local hex = {}
+hex['0'] = '3F'
+hex['1'] = '06'
+hex['2'] = '5B'
+hex['3'] = '4F'
+hex['4'] = '66'
+hex['5'] = '6D'
+hex['6'] = '7D'
+hex['7'] = '07'
+hex['8'] = '7F'
+hex['9'] = '6F'
+hex['A'] = '77'
+hex['B'] = '7C'
+hex['C'] = '39'
+hex['D'] = '5E'
+hex['E'] = '79'
+hex['F'] = '71'
+hex['G'] = '3D'
+hex['H'] = '76'
+hex['I'] = '30'
+hex['J'] = '1E'
+hex['K'] = '75'
+hex['L'] = '38'
+hex['M'] = '37'
+hex['N'] = '54'
+hex['O'] = '3F'
+hex['P'] = '73'
+hex['Q'] = '67'
+hex['R'] = '50'
+hex['S'] = '6D'
+hex['T'] = '78'
+hex['U'] = '3E'
+hex['V'] = '1C'
+hex['W'] = '2A'
+hex['X'] = '76'
+hex['Y'] = '6E'
+hex['Z'] = '5B'
+
+function hexToDecimal(decimal, hex)
+  for key, value in pairs(hex) do
+    decimal[key] = tonumber(value, 16)
+  end
+  return decimal
 end
 
-function main()
+local decimal = {}
+hexToDecimal(decimal, hex)
+
+function decimalContains(decimal, key)
+  return decimal[key] ~= nil
+end
+
+function setTwoSegments(left_char, right_char)
+  shifted_1st_char = bit.blshift(decimal[left_char], 8)
+  final_2_chars = shifted_1st_char + decimal[right_char]
+  rs.setBundledOutput(bundledOutputSide, final_2_chars)
+end
+
+function enterTwoChars()
   term.clear()
   term.setCursorPos(1,1)
   write('Left char: ')
@@ -31,16 +70,32 @@ function main()
   write('Right char: ')
   right_char = read()
 
-  if (binaryContains(left_char) and binaryContains(right_char)) then
-    shifted_1st_char = bit.blshift(binary[left_char], 8)
-    final_2_chars = shifted_1st_char + binary[right_char]
-
-    rs.setBundledOutput(bundledOutputSide, final_2_chars)
+  if (decimalContains(decimal, left_char) and decimalContains(decimal, right_char)) then
+    setTwoSegments(left_char, right_char)
   else
     print('You entered an invalid character!')
-    sleep(3)
+    sleep(2)
     main()
   end
+end
+
+function setLeftSegment(left_char)
+  char = bit.blshift(decimal[left_char], 8)
+  rs.setBundledOutput(bundledOutputSide, char)
+end
+
+function scrollAllChars()
+  while true do
+    for key, value in pairs(decimal) do      
+      setLeftSegment(key)
+      sleep(0.5)
+    end
+  end
+end
+
+function main()
+  -- enterTwoChars()
+  scrollAllChars()
 end
 
 main()
