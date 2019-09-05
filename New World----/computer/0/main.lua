e5e908340aef9edd47e598bb4c3cf25440f7dc05
MyNameIsTrez <32989873+MyNameIsTrez@users.noreply.github.com>
2019-09-05 13:15:38 +0200

    Now using one array with an index and a hex, and a string with chars that are looked at to get the index.

--- New World----/computer/0/main.lua
+++ New World----/computer/0/main.lua
@@ -1,97 +1,71 @@
 -- Made by MyNameIsTrez in 2019
 bundledOutputSide = 'back'
 
--- local numbers = '0123456789'
--- local alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
 local hex = {}
-hex['0'] = '3F'
-hex['1'] = '06'
-hex['2'] = '5B'
-hex['3'] = '4F'
-hex['4'] = '66'
-hex['5'] = '6D'
-hex['6'] = '7D'
-hex['7'] = '07'
-hex['8'] = '7F'
-hex['9'] = '6F'
-hex['A'] = '77'
-hex['B'] = '7C'
-hex['C'] = '39'
-hex['D'] = '5E'
-hex['E'] = '79'
-hex['F'] = '71'
-hex['G'] = '3D'
-hex['H'] = '76'
-hex['I'] = '30'
-hex['J'] = '1E'
-hex['K'] = '75'
-hex['L'] = '38'
-hex['M'] = '37'
-hex['N'] = '54'
-hex['O'] = '3F'
-hex['P'] = '73'
-hex['Q'] = '67'
-hex['R'] = '50'
-hex['S'] = '6D'
-hex['T'] = '78'
-hex['U'] = '3E'
-hex['V'] = '1C'
-hex['W'] = '2A'
-hex['X'] = '76'
-hex['Y'] = '6E'
-hex['Z'] = '5B'
+hex[1] = '3F'
+hex[2] = '06'
+hex[3] = '5B'
+hex[4] = '4F'
+hex[5] = '66'
+hex[6] = '6D'
+hex[7] = '7D'
+hex[8] = '07'
+hex[9] = '7F'
+hex[10] = '6F'
+hex[11] = '77'
+hex[12] = '7C'
+hex[13] = '39'
+hex[14] = '5E'
+hex[15] = '79'
+hex[16] = '71'
+hex[17] = '3D'
+hex[18] = '76'
+hex[19] = '30'
+hex[20] = '1E'
+hex[21] = '75'
+hex[22] = '38'
+hex[23] = '37'
+hex[24] = '54'
+hex[25] = '3F'
+hex[26] = '73'
+hex[27] = '67'
+hex[28] = '50'
+hex[29] = '6D'
+hex[30] = '78'
+hex[31] = '3E'
+hex[32] = '1C'
+hex[33] = '2A'
+hex[34] = '76'
+hex[35] = '6E'
+hex[36] = '5B'
 
 local chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-function hexToDecimal(decimal, hex)
-  for key, value in pairs(hex) do
-    decimal[key] = tonumber(value, 16)
-  end
-  return decimal
+
+function setTwoSegments(left_hex, right_hex)
+  local left_decimal = tonumber(left_hex, 16)
+  local right_decimal = tonumber(right_hex, 16)
+  
+  shifted_left_char = bit.blshift(left_decimal, 8)
+  combined_chars = shifted_left_char + right_decimal
+
+  rs.setBundledOutput(bundledOutputSide, combined_chars)
 end
 
-local decimal = {}
-hexToDecimal(decimal, hex)
-
-function decimalContains(decimals, left_char, right_char)
-  -- return decimal[char] ~= nil
-  -- whether decimal contains left_char and right_char
-  local contains_left = nil
-  local contains_right = nil
-
-
-  -- ADD SUBTABLES TO DECIMALS INSTEAD OF HOW IT IS ONE TABLE RN
-
-
-  -- gets the index of left_char and right_char in decimals
-  for index, subtable in ipairs(decimals) do
-    -- print(subtable[left_char])
-    if subtable[left_char] ~= nil then
-      contains_left = index
-    end
-    -- print(subtable[right_char])
-    if subtable[right_char] ~= nil then
-      contains_right = index
+function getHex(char)
+  _hex = nil
+  -- check if the char is in chars and get the hex
+  for i = 1, #chars do
+    if string.sub(chars, i, i) == char then
+      _hex = hex[i]
     end
   end
-
-  -- print(left_char)
-  -- print(right_char)
-  -- print(contains_left)
-  -- print(contains_right)
-  -- sleep(1)
-
-  return contains_left ~= nil and contains_right ~= nil 
-end
-
-function setTwoSegments(left_char, right_char)
-  shifted_1st_char = bit.blshift(decimal[left_char], 8)
-  final_2_chars = shifted_1st_char + decimal[right_char]
-  rs.setBundledOutput(bundledOutputSide, final_2_chars)
+  -- return the hex
+  return _hex
 end
 
 function enterTwoChars()
+  -- ready the terminal
   term.clear()
   term.setCursorPos(1,1)
   
@@ -101,8 +75,14 @@ function enterTwoChars()
   write('Right char: ')
   right_char = read()
 
-  if (decimalContains(decimal, left_char, right_char)) then
-    setTwoSegments(left_char, right_char)
+  left_hex = getHex(left_char)
+  right_hex = getHex(right_char)
+  correct_input = left_hex and right_hex
+
+  -- check if the two entered chars are valid
+  if (correct_input) then
+    print('Success!')
+    setTwoSegments(left_hex, right_hex)
   else
     print('You entered an invalid character!')
     sleep(2)
@@ -110,23 +90,56 @@ function enterTwoChars()
   end
 end
 
-function setLeftSegment(left_char)
-  char = bit.blshift(decimal[left_char], 8)
-  rs.setBundledOutput(bundledOutputSide, char)
-end
-
-function scrollAllChars()
-  while true do
-    for key, value in pairs(decimal) do      
-      setLeftSegment(key)
-      sleep(0.5)
-    end
-  end
-end
-
 function main()
   enterTwoChars()
-  -- scrollAllChars()
 end
 
 main()
+
+
+
+
+
+
+
+-- function hexToDecimal(decimal, hex)
+--   for key, value in pairs(hex) do
+--     decimal[key] = tonumber(value, 16)
+--   end
+--   return decimal
+-- end
+
+-- local decimal = {}
+-- hexToDecimal(decimal, hex)
+
+-- function decimalContains(decimals, left_char, right_char)
+--   -- whether decimal contains left_char and right_char
+--   local contains_left = nil
+--   local contains_right = nil
+
+--   -- gets the index of left_char and right_char in decimals
+--   for index, subtable in ipairs(decimals) do
+--     if subtable[left_char] ~= nil then
+--       contains_left = index
+--     end
+--     if subtable[right_char] ~= nil then
+--       contains_right = index
+--     end
+--   end
+
+--   return contains_left ~= nil and contains_right ~= nil 
+-- end
+
+-- function setLeftSegment(left_char)
+--   char = bit.blshift(decimal[left_char], 8)
+--   rs.setBundledOutput(bundledOutputSide, char)
+-- end
+
+-- function scrollAllChars()
+--   while true do
+--     for key, value in pairs(decimal) do      
+--       setLeftSegment(key)
+--       sleep(0.5)
+--     end
+--   end
+-- end
