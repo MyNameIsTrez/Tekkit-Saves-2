3c6383b8290d706a4ac9651f71b287d2ad06f574
MyNameIsTrez <32989873+MyNameIsTrez@users.noreply.github.com>
2019-09-17 18:53:31 +0200

    Added a GUI on the right and added potential and kinetic energy calculation.

--- New World----/computer/8/physics_simulation.lua
+++ New World----/computer/8/physics_simulation.lua
@@ -9,6 +9,18 @@
 -- 227:78 on the laptop in windowed mode.
 -- 227:85 on the laptop in fullscreen mode.
 
+local w, h = term.getSize()
+
+local gui_x = w/4*3 -- The x value of the left line of the GUI.
+
+local entity_min_x = 2
+local entity_max_x = gui_x-1
+local entity_min_y = 2
+local entity_max_y = h-2
+
+local g = 9.81 -- m/s^2. Gravitational acceleration.
+local tick_speed = 10
+
 function loadAPIs()
   -- Makes a table of the IDs and names of the APIs to load.
   local APIs = {
@@ -26,49 +38,116 @@ function loadAPIs()
 end
 loadAPIs()
 
-local w, h = term.getSize()
-
 function clear()
   term.clear()
   term.setCursorPos(1,1)
 end
 
 local corners = {
-  {
+  { -- Top-left.
     x = 1,
     y = 1
   },
-  {
+  { -- Bottom-right.
     x = w-1,
     y = h-1
   }
 }
 
+local gui_corner = {
+  x = gui_x,
+  y = 1
+}
+
 local entities = {
   {
-    x = w/2,
-    y = h/2
+    x = w/2, -- m offset.
+    y = h/2, -- m offset.
+    x_speed = 0, -- m/s.
+    y_speed = 1, -- m/s.
+    speed = 0, -- m/s. pythagoras of x_speed and y_speed.
+    mass = 0.25, -- kg.
+    energy_potential = 0 -- J.
   }
 }
 
-function entities:move(i, x_diff, y_diff)
-  local e = entities[i]
-  local not_touching_wall = true
-  if (not_touching_wall) then
-    shape.point(e, false)
-    e.x = e.x + x_diff
-    e.y = e.y + y_diff
-    shape.point(e)
+function writeGUI(string, height)
+  term.setCursorPos(gui_x + 2, 1 + 2 * height)
+  write("                                                     ")
+  term.setCursorPos(gui_x + 2, 1 + 2 * height)
+  write(string)
+end
+
+function pythagoras(a, b)
+  return math.sqrt(math.pow(a, 2) + math.pow(b, 2))
+end
+
+function entities:calcEnergyPotential(i)
+  local entity = entities[i]
+
+  local h = entity_max_y - entity.y
+  entity.energy_potential = entity.mass * g * h
+
+  writeGUI("h: "..h, 1)
+  writeGUI("entity.energy_potential: "..entity.energy_potential.." J", 2)
+end
+
+function entities:calcEnergyKinetic(i)
+  local entity = entities[i]
+
+  entity.speed = pythagoras(entity.x_speed, entity.y_speed)
+  entity.energy_kinetic = 0.5 * entity.mass * math.pow(entity.speed, 2)
+
+  writeGUI("entity.x_speed: "..entity.x_speed.." m/s", 3)
+  writeGUI("entity.y_speed: "..entity.y_speed.." m/s", 4)
+  writeGUI("entity.speed: "..entity.speed.." m/s", 5)
+  writeGUI("entity.energy_kinetic: "..entity.energy_kinetic.." J", 6)
+end
+
+function entities:move(i)
+  local entity = entities[i]
+
+  local dist_wall_north = entity_min_y - entity.y
+  local dist_wall_east = entity_max_x - entity.x
+  local dist_wall_south = entity_max_y - entity.y
+  local dist_wall_west = entity_min_x - entity.x
+
+  if (entity.y_speed < dist_wall_north) then
+    entity.y_speed = dist_wall_north
   end
+  if (entity.x_speed > dist_wall_east) then
+    entity.x_speed = dist_wall_east
+  end
+  if ( entity.y_speed > dist_wall_south) then
+    entity.y_speed = dist_wall_south
+  end
+  if (entity.x_speed < dist_wall_west) then
+    entity.x_speed = dist_wall_west
+  end
+
+  -- Remove the currently drawn position of the entity.
+  shape.point(entity, " ")
+  entity.x = entity.x + entity.x_speed
+  entity.y = entity.y + entity.y_speed
+  -- Draw the new position of the entity.
+  shape.point(entity, "#")
+end
+
+function drawGUI()
+  shape.rectangle(corners[1], corners[2])
+  shape.rectangle(gui_corner, corners[2])
 end
 
 function main()
-  shape.point(entities[1])
+  clear()
+  drawGUI()
+  shape.point(entities[1], "#")
+
   while true do
-    clear()
-    shape.rectangle(corners[1], corners[2])
-    entities:move(1, 1, 1)
-    sleep(1)
+    entities:move(1)
+    entities:calcEnergyPotential(1)
+    entities:calcEnergyKinetic(1)
+    sleep(1 / tick_speed)
   end
 end
 main()
