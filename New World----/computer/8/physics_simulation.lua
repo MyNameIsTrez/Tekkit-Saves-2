7eb02e45da93b1de96a031d5caaf680b982303c3
MyNameIsTrez <32989873+MyNameIsTrez@users.noreply.github.com>
2019-09-17 20:15:58 +0200

    Got the basic mechanic of energy conservation working.

--- New World----/computer/8/physics_simulation.lua
+++ New World----/computer/8/physics_simulation.lua
@@ -18,8 +18,8 @@ local entity_max_x = gui_x-1
 local entity_min_y = 2
 local entity_max_y = h-2
 
-local g = 9.81 -- m/s^2. Gravitational acceleration.
-local tick_speed = 10
+local g = 9.81 -- m/s^2 or N/kg. Gravitational acceleration.
+local tick_speed = 1
 
 function loadAPIs()
   -- Makes a table of the IDs and names of the APIs to load.
@@ -64,10 +64,13 @@ local entities = {
     x = w/2, -- m offset.
     y = h/2, -- m offset.
     x_speed = 0, -- m/s.
-    y_speed = 1, -- m/s.
-    speed = 0, -- m/s. pythagoras of x_speed and y_speed.
+    y_speed = 0, -- m/s.
+    facing = 1.5 * math.pi, -- radians, setting it to face south.
+    speed_vertical = 0, -- m/s. pythagoras of x_speed and y_speed.
     mass = 0.25, -- kg.
-    energy_potential = 0 -- J.
+    energy_total = 0, -- J. This value gets set in calcEnergies().
+    energy_potential = 0, -- J.
+    energy_kinetic = 0 -- J. Result of energy_total - energy_potential.
   }
 }
 
@@ -82,30 +85,34 @@ function pythagoras(a, b)
   return math.sqrt(math.pow(a, 2) + math.pow(b, 2))
 end
 
-function entities:calcEnergyPotential(i)
+function entities:calcEnergy(i)
   local entity = entities[i]
 
   local h = entity_max_y - entity.y
   entity.energy_potential = entity.mass * g * h
+  entity.energy_kinetic = entity.energy_total - entity.energy_potential
+  entity.energy_total = entity.energy_potential + entity.energy_kinetic
 
   writeGUI("h: "..h, 1)
   writeGUI("entity.energy_potential: "..entity.energy_potential.." J", 2)
+  writeGUI("entity.energy_kinetic: "..entity.energy_kinetic.." J", 3)
+  writeGUI("entity.energy_total: "..entity.energy_total.." J", 4)
 end
 
-function entities:calcEnergyKinetic(i)
+function entities:calcSpeed(i)
   local entity = entities[i]
 
-  entity.speed = pythagoras(entity.x_speed, entity.y_speed)
-  entity.energy_kinetic = 0.5 * entity.mass * math.pow(entity.speed, 2)
+  -- Ek = 0.5 * m * v^2
+  -- v = math.sqrt(Ek / 0.5 / m)
+  entity.speed_vertical = math.sqrt(entity.energy_kinetic / 0.5 / entity.mass)
 
-  writeGUI("entity.x_speed: "..entity.x_speed.." m/s", 3)
-  writeGUI("entity.y_speed: "..entity.y_speed.." m/s", 4)
-  writeGUI("entity.speed: "..entity.speed.." m/s", 5)
-  writeGUI("entity.energy_kinetic: "..entity.energy_kinetic.." J", 6)
+  writeGUI("entity.speed_vertical: "..entity.speed_vertical.." m/s", 5)
 end
 
 function entities:move(i)
   local entity = entities[i]
+
+  entity.y_speed = entity.speed_vertical
 
   local dist_wall_north = entity_min_y - entity.y
   local dist_wall_east = entity_max_x - entity.x
@@ -139,15 +146,22 @@ function drawGUI()
 end
 
 function main()
+  local entity = entities[1]
+  local h = entity_max_y - entity.y
+  -- We need to make the total energy a tiny bit larger than the potential energy
+  -- for the entities to start moving. I should fix this somehow.
+  local tiny = 0.000001
+  entity.energy_total = entity.mass * g * h + tiny
+
   clear()
   drawGUI()
   shape.point(entities[1], "#")
 
   while true do
     entities:move(1)
-    entities:calcEnergyPotential(1)
-    entities:calcEnergyKinetic(1)
-    sleep(1 / tick_speed)
+    entities:calcEnergy(1)
+    entities:calcSpeed(1)
+    sleep(1/tick_speed)
   end
 end
 main()
