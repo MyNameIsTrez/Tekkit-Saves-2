ea3b1fdad0fb86085b89006c96cbb88616cbddcc
MyNameIsTrez <32989873+MyNameIsTrez@users.noreply.github.com>
2019-12-01 15:20:51 +0100

    Implementing A*.

--- Random Projects/computer/15/main.lua
+++ Random Projects/computer/15/main.lua
@@ -29,8 +29,10 @@ local entityCount = 2
 
 -- UNEDITABLE VARIABLES --------------------------------------------------------
 
-local entities = {}
 local w, h = term.getSize()
+w = w - 1
+local entities = {}
+local nodes = {}
 
 
 
@@ -38,22 +40,15 @@ local w, h = term.getSize()
 
 Entity = {
 
-	new = function(self, id, x, y, wall)
-		local starting_values = {
+	new = function(self, id, x, y)
+		local startingValues = {
 			id = id,
 			pos = vector.new(x, y),
 			icon = "e",
-
-			-- Pathfinding.
-			targetId,
-			neighbors,
-			f = 0,
-			g = 0,
-			h = 0,
-			wall = wall
+			targetId
 		}
-		setmetatable(starting_values, {__index = self})
-		return starting_values
+		setmetatable(startingValues, {__index = self})
+		return startingValues
 	end,
 
 	show = function(self)
@@ -62,63 +57,64 @@ Entity = {
 
 	pathfind = function(self)
 		local openSet = {}
-		print(self.pos)
-		-- sleep(10)
-		openSet[1] = self.pos
+		openSet[1] = nodes[self.pos.x][self.pos.y]
 
 		local closedSet = {}
 
 		while true do
 			if #openSet >= 1 then
-				local currentIndex = 0
+				local currentIndex = 1
 				for i = 1, #openSet do
-					if openSet[i].f[self.id] < openSet[currentIndex].f[self.id] then
-						currentIndex = i
+					if openSet[i].f[self.id] and openSet[currentIndex].f[self.id] then
+						if openSet[i].f[self.id] < openSet[currentIndex].f[self.id] then
+							currentIndex = i
+						end
 					end
 				end
-				local furthest = openSet[currentIndex]
+				local furthestNode = openSet[currentIndex]
 
 				-- Found a solution.
-				if furthest == targetPos then
+				local target = entities[self.targetId]
+				local targetNode = nodes[target.pos.x][target.pos.y]
+				if furthestNode == targetNode then
 					return
 				end
 
-				self:removeFromTable(openSet, furthest)
-				closedSet[#closedSet + 1] = furthest
+				self:removeFromTable(openSet, furthestNode)
+				closedSet[#closedSet + 1] = furthestNode
 
-				local neighbors = furthest.neighbors
-				for i = 1, #neighbors do
-					local neighbor = neighbors[i]
+				for i = 1, #furthestNode.neighborNodes do
+					local neighborNode = furthestNode.neighborNodes[i]
 
-					if not self:tableContains(closedSet, neighbor) and not neighbor.wall then
+					if not self:tableContains(closedSet, neighborNode) and not neighborNode.impassable then
 						-- I removed 'n / tileSizeFull', because each tile is 1 wide and high.
-						local heur = self:heuristic(neighbor, furthest) / 1
+						local heur = self:heuristic(neighborNode, furthestNode) / 1
 						
 						local tempG
 						-- If the 'g' property exists, add it. Otherwise, keep it at 0.
-						if furthest.g[self.id] + heur then
-							tempG = furthest.g[self.id] + heur
+						if furthestNode.g[self.id] + heur then
+							tempG = furthestNode.g[self.id] + heur
 						else
 							tempG = heur
 						end
 
 						local newPath = false
-						if self:tableContains(openSet, neighbor) then
-							if tempG < neighbor.g[self.id] then
-								neighbor.g[self.id] = tempG
+						if self:tableContains(openSet, neighborNode) then
+							if tempG < neighborNode.g[self.id] then
+								neighborNode.g[self.id] = tempG
 								newPath = true
 							end
 						else
-							neighbor.g[self.id] = tempG
-							openSet[#openSet + 1] = neighbor
+							neighborNode.g[self.id] = tempG
+							openSet[#openSet + 1] = neighborNode
 							newPath = true
 						end
 
 						if newPath then
-							-- What is targetPos exactly?
-							neighbor.h[self.id] = self:heuristic(neighbor, targetPos)
-							neighbor.f[self.id] = neighbor.g[self.id] + neighbor.h[self.id]
-							neighbor.parents[self.id] = furthest
+							-- What is targetNode exactly?
+							neighborNode.h[self.id] = self:heuristic(neighborNode, targetNode)
+							neighborNode.f[self.id] = neighborNode.g[self.id] + neighborNode.h[self.id]
+							neighborNode.parents[self.id] = furthestNode
 						end
 					end
 				end
@@ -153,6 +149,46 @@ Entity = {
 
 }
 
+Node = {
+
+	new = function(self, x, y, impassable)
+		local startingValues = {
+			pos = vector.new(x, y),
+			impassable = impassable,
+
+			neighborNodes = {},
+			f = {}, -- g + h.
+			g = {}, -- Cost from the start.
+			h = {} -- Minimum cost to the end.
+		}
+		setmetatable(startingValues, {__index = self})
+		return startingValues
+	end,
+
+	setNeighborNodes = function(self)
+		-- Top.
+		if self.pos.y > 1 then
+			self.neighborNodes[#self.neighborNodes + 1] = nodes[self.x][self.y - 1]
+		end
+
+		-- Left.
+		if self.pos.x > 1 then
+			self.neighborNodes[#self.neighborNodes + 1] = nodes[self.x - 1][self.y]
+		end
+
+		-- Bottom.
+		if self.pos.y < h then
+			self.neighborNodes[#self.neighborNodes + 1] = nodes[self.x][self.y + 1]
+		end
+
+		-- Right.
+		if self.pos.x < w then
+			self.neighborNodes[#self.neighborNodes + 1] = nodes[self.x + 1][self.y]
+		end
+	end
+
+}
+
 
 
 -- FUNCTIONS --------------------------------------------------------
@@ -160,7 +196,25 @@ Entity = {
 -- Enemies.
 function createEntities()
 	for id = 1, entityCount do
-		entities[id] = Entity:new(id, math.random(w), math.random(h), false)
+		entities[id] = Entity:new(id, math.random(w), math.random(h))
+	end
+end
+
+
+function createNodes()
+	for x = 1, w do
+		nodes[x] = {}
+		for y = 1, h do
+			local impassable = math.random(0, 1)
+			nodes[x][y] = Node:new(x, y, impassable)
+		end
+	end
+
+	-- This loop can be done with pairs() or ipairs() later.
+	for x = 1, w do
+		for y = 1, h do
+			nodes[x][y]:setNeighborNodes()
+		end
 	end
 end
 
@@ -170,8 +224,11 @@ end
 
 function setup()
 	term.clear()
+
 	createEntities()
 	entities[1].targetId = 2
+	
+	createNodes()
 end
 
 
