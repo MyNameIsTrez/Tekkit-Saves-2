59f334750b705b911552cf3ee689c01993150467
MyNameIsTrez <32989873+MyNameIsTrez@users.noreply.github.com>
2019-12-20 02:15:56 +0100

    Unexplainable problem: delimiterIndex should be parsing searchedString which has around 6 characters, but the :find function returns an index of 2000+.

--- Random Projects/computer/19/main.lua
+++ Random Projects/computer/19/main.lua
@@ -44,45 +44,108 @@ local frameWidth
 local frameHeight
 local frameCount
 
-local optimizedFramesString
-local optimizedFrames
+local unpackedOptimizedFrames = {}
 
 local previousClock = 0
 
 -- FUNCTIONS --------------------------------------------------------
 
+local function tryYield()
+    local currentClock = os.clock()
+    if currentClock - previousClock > 4 then
+        previousClock = currentClock
+        cf.yield()
+    end
+end
+
+local function unpackOptimizedFrames(optimizedFrames)
+    print('Unpacking optimized frames...')
+    
+    cursorX, cursorY = term.getCursorPos()
+    for f = 1, frameCount do
+        if f % 10 == 0 then
+            term.setCursorPos(cursorX, cursorY) -- Used so the next 'Creating frame' print statement can overwrite itself in its for loop.
+            print('Creating frame '..tostring(f)..'/'..tostring(frameCount)..'.')
+        end
+
+        local unoptimizedString = ''
+        local optimizedFrame = optimizedFrames[f]
+        
+        local openBracketIndex = nil -- Probably not necessary to assign this with 'nil'.
+
+        -- This can probably be done faster using table.concat:
+        -- https://stackoverflow.com/a/1407187
+        for currentIndex = 1, #optimizedFrame do
+            local char = optimizedFrame:sub(currentIndex, currentIndex) -- can't use pairs or ipairs, I think
+            
+            if char == '[' then
+                openBracketIndex = currentIndex
+            end
+            
+            if openBracketIndex == nil then
+                unoptimizedString = unoptimizedString .. char
+            end
+            
+            if char == ']' then
+                -- More efficient to look at 'optimizedFrame' once like this, because we need it multiple times.
+                local searchedString = optimizedFrame:sub(openBracketIndex, currentIndex)
+                
+                print('searchedString: '..searchedString)
+                print('#searchedString: '..tostring(#searchedString))
+                print('openBracketIndex: '..tostring(openBracketIndex))
+                local delimiterIndex = searchedString:find(';')
+                
+                -- Get the number of times to repeat the group of characters.
+                local repetition = tonumber(searchedString:sub(2, delimiterIndex - openBracketIndex))
+                -- print('repetition: '..tostring(repetition))
+                
+                -- Get the repeated characters.
+                local searchedStringLen = currentIndex - delimiterIndex - 1 -- Because 5 - 3 - 1 -> 1.
+                print('searchedStringLen: '..tostring(searchedStringLen))
+                local repeatedChars = searchedString:sub(delimiterIndex + 1 - openBracketIndex, currentIndex - 1 - openBracketIndex)
+                print('delimiterIndex: '..tostring(delimiterIndex))
+                print('currentIndex: '..currentIndex)
+                print('repeatedChars: '..tostring(repeatedChars))
+                
+                -- Add the repeated characters.
+                unoptimizedString = unoptimizedString .. repeatedChars:rep(repetition)
+                
+                openBracketIndex = nil
+            end
+        end
+
+        table.insert(unpackedOptimizedFrames, unoptimizedString)
+
+        if f % 10 == 0 then
+            tryYield()
+        end
+    end
+end
+
 local function getSelectedAnimationData()
 	if cfg.computerType ~= "laptop" and cfg.computerType ~= "desktop" then
 		error("You didn't enter a valid 'computerType' name in the cfg file!")
 	end
-	print("Loading animation...")
+	print("Loading animation data...")
 	local file = fs.open(cfg.computerType.." inputs/"..cfg.fileName..".txt", "r")
 	local string = file.readAll()
 	file.close()
-
+    
 	tab = textutils.unserialize(string)
-
+    
 	frameWidth = tab.width
 	frameHeight = tab.height
     frameCount = tab.frame_count
     
-    optimizedFramesString = tab.optimized_frames
-end
-
-local function tryYield()
-	local currentClock = os.clock()
-	if currentClock - previousClock > 4 then
-		previousClock = currentClock
-		cf.yield()
-	end
+    local optimizedFrames = tab.optimized_frames
+    unpackOptimizedFrames(optimizedFrames)
 end
 
 local function dataToGeneratedCode()
-	print("Converting data to the optimized frames...")
-    optimizedFrames = {}
+	print("Generating executable code...")
     frameSize = frameWidth * frameHeight
 
-    local handle = io.open("generatedCode", "w")
+    local handle = io.open("generated code", "w")
 
     if handle then
         whileLoop = frameCount > 1 and cfg.loop
@@ -95,13 +158,9 @@ local function dataToGeneratedCode()
         end
 
         for f = 1, frameCount do
-            index1 = (f - 1) * frameSize + 1
-            index2 = f * frameSize
-            frameString = optimizedFramesString:sub(index1, index2)
-
             local string =
             '\n        term.setCursorPos(1,1)'..
-            '\n        write("'..frameString..'")'..
+            '\n        write("'..unpackedOptimizedFrames[f]..'")'..
             '\n        os.queueEvent("r")'..
             '\n        os.pullEvent("r")'
 
@@ -126,7 +185,7 @@ local function dataToGeneratedCode()
             handle:write(string)
         end
     else
-        error("couldn't create/open the generatedCode file")
+        error("couldn't create/open the generated code file")
     end
     
     handle:close()
@@ -145,13 +204,12 @@ local function setup()
 		tryYield()
 		term.clear()
 		term.setCursorPos(1, 1)
-        -- sleep(2)
     end
 end
 
 local function main()
 	if not rs.getInput(cfg.leverSide) then
-        shell.run("generatedCode")
+        shell.run("generated code")
         term.setCursorPos(width, height)
 	end
 end
