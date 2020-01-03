9775399a56139380f9bdbed8f71dd4d889947bdb
MyNameIsTrez <32989873+MyNameIsTrez@users.noreply.github.com>
2020-01-03 22:50:37 +0100

    Version 1.5 - All functions have been put into the Animation class, and the class will be transported to Pastebin.

--- Random Projects/computer/19/main.lua
+++ Random Projects/computer/19/main.lua
@@ -1,348 +1,313 @@
 -- README --------------------------------------------------------
 
--- Program used to create and preview animations/images.
+-- Program used to create and preview dithered animations/images.
 
--- The default terminal ratio is 25:9, which is absolutely tiny.
--- To get the terminal to fill the entire screen, use these widths and heights:
-	-- My 31.5" monitor:
-		-- 426:153 in windowed mode.
-		-- 426:160 in fullscreen mode.
-		-- 200:70 in windowed mode with GUI Scale: Normal. (for debugging)
-	-- My laptop:
-		-- 227:78 in windowed mode.
-		-- 227:85 in fullscreen mode.
+-- The default terminal ratio is 25:9.
 
--- IMPORTING --------------------------------------------------------
+-- To get the terminal to fill your entire monitor and to get a custom text color:
+-- 1. Open AppData/Roaming/.technic/modpacks/tekkit/config/mod_ComputerCraft.cfg
+-- 2. Divide your monitor's width by 6 and your monitor's height by 9. 
+-- 3. Set terminal_width to the calculated width and do the same for terminal_height.
+-- 4. Set the terminal_textColour_r, terminal_textColour_g and terminal_textColour_b
+--    to values between 0 and 255 to get a custom text color.
 
-local function importConfig()
-    os.loadAPI('cfg')
-end
+-- ANIMATION CLASS --------------------------------------------------------
 
-local function importAPIs()
-	local APIs = {
-		{id = 'p9tSSWcB', name = 'cf'},
-	}
+Animation = {
 
-	fs.delete('apis') -- Deletes the folder, with every API file in it.
-	fs.makeDir('apis') -- Recreates the folder.
+	new = function(self)
+		local startingValues = {
+			frameCount,
+			compressed,
+			unpackedOptimizedFrames = {}
+		}
+		
+		setmetatable(startingValues, {__index = self})
+		return startingValues
+	end,
 
-	for _, API in pairs(APIs) do
-		shell.run('pastebin', 'get', API.id, 'apis/' .. API.name)
-		os.loadAPI('apis/' .. API.name)
-	end
-end
+	-- IMPORTING --------------------------------------------------------
 
--- Request animation using http calls.
--- local function importAnimation()
--- end
+	importConfig = function(self)
+		os.loadAPI('cfg')
+	end,
 
--- NOT USER EDITABLE VARIABLES --------------------------------------------------------
+	importAPIs = function(self)
+		local APIs = {
+			{id = 'p9tSSWcB', name = 'cf'}, -- Common Functions.
+		}
 
-local width, height = term.getSize()
-width = width - 1
+		fs.delete('apis') -- Deletes the folder, with every API file in it.
+		fs.makeDir('apis') -- Recreates the folder.
 
-local frameCount
+		for _, API in pairs(APIs) do
+			shell.run('pastebin', 'get', API.id, 'apis/' .. API.name)
+			os.loadAPI('apis/' .. API.name)
+		end
+	end,
 
-local compressed
+	-- Request animation using http calls.
+	-- importAnimationData = function(self)
+	-- end,
 
-local unpackedOptimizedFrames = {}
+	-- FUNCTIONS --------------------------------------------------------
 
--- FUNCTIONS --------------------------------------------------------
+	unpackOptimizedFrames = function(self, optimizedFrames)
+		print('Unpacking optimized frames...')
+		
+		startTime = os.clock()
+		local cursorX, cursorY = term.getCursorPos()
+		for f = 1, self.frameCount do
+			-- Print speed statistics.
+			if f % 10 == 0 then
+				-- Progress.
+				progress = 'Creating frame ' .. tostring(f) .. '/' .. tostring(self.frameCount)
 
-local function unpackOptimizedFrames(optimizedFrames)
-    print('Unpacking optimized frames...')
-	
-	startTime = os.clock()
-	local cursorX, cursorY = term.getCursorPos()
-	for f = 1, frameCount do
-		-- Print speed statistics.
-		if f % 10 == 0 then
-			-- Progress.
-			progress = 'Creating frame ' .. tostring(f) .. '/' .. tostring(frameCount)
-
-			-- Speed.
-			elapsed = (os.clock() - startTime) / 10
-			processedFps = 1 / elapsed
-			speed = string.format('%d frames/s', processedFps)
-			
-			-- ETA.
-			framesLeft = frameCount - f
-			etaMinutes = math.floor(elapsed * framesLeft / 60)
-			etaSeconds = math.floor(elapsed * framesLeft) % 60
-			eta = string.format('%dm, %ds left', etaMinutes, etaSeconds)
-			
-			-- Clear.
-			clear = '		'
-
-			term.setCursorPos(cursorX, cursorY) -- Used so the next 'Creating frame' print statement can overwrite itself in its for loop.
-            print(progress .. ', ' .. speed .. ', ' .. eta .. clear)
-			startTime = os.clock()
-        end
-
-        local unoptimizedString = ''
-        local optimizedFrame = optimizedFrames[f]
-        
-        local openBracketIndex = nil -- Probably not necessary to assign this with 'nil'.
-
-		-- This can probably be done faster using table.concat:
-		-- https://stackoverflow.com/a/1407187
-
-		for currentIndex = 1, #optimizedFrame do
-			local char = optimizedFrame:sub(currentIndex, currentIndex) -- can't use pairs or ipairs, I think
-			
-			if char == '[' then
-				openBracketIndex = currentIndex
-			end
-			
-			if openBracketIndex == nil then
-				unoptimizedString = unoptimizedString .. char
-			end
-			
-			if char == ']' then
-				-- More efficient to look at 'optimizedFrame' once like this, because we 'sub:' it twice later.
-				local closedBrackedIndex = currentIndex
-				local searchedString = optimizedFrame:sub(openBracketIndex, closedBrackedIndex)
+				-- Speed.
+				elapsed = (os.clock() - startTime) / 10
+				processedFps = 1 / elapsed
+				speed = string.format('%d frames/s', processedFps)
 				
-				-- Get the repeated characters.
-				-- local delimiterIndex = searchedString:find(';')
+				-- ETA.
+				framesLeft = self.frameCount - f
+				etaMinutes = math.floor(elapsed * framesLeft / 60)
+				etaSeconds = math.floor(elapsed * framesLeft) % 60
+				eta = string.format('%dm, %ds left', etaMinutes, etaSeconds)
+				
+				-- Clear.
+				clear = '		'
 
-				local delimiterIndex
-				for i = 1, #searchedString do
-					local char = searchedString:sub(i, i)
-					if char == ';' then
-						delimiterIndex = i
-						break
-					end
+				term.setCursorPos(cursorX, cursorY) -- Used so the next 'Creating frame' print statement can overwrite itself in its for loop.
+				print(progress .. ', ' .. speed .. ', ' .. eta .. clear)
+				startTime = os.clock()
+			end
+
+			local unoptimizedString = ''
+			local optimizedFrame = optimizedFrames[f]
+			
+			local openBracketIndex = nil -- Probably not necessary to assign this with 'nil'.
+
+			-- This can probably be done faster using table.concat:
+			-- https://stackoverflow.com/a/1407187
+
+			for currentIndex = 1, #optimizedFrame do
+				local char = optimizedFrame:sub(currentIndex, currentIndex) -- can't use pairs or ipairs, I think
+				
+				if char == '[' then
+					openBracketIndex = currentIndex
 				end
-
-				-- print('delimiterIndex: '..tostring(delimiterIndex))
-				-- sleep(1)
-
-				local searchedDelimiterIndex = delimiterIndex + 2 - openBracketIndex
-				local searchedClosedBracketIndex = closedBrackedIndex - openBracketIndex
-				local repeatedChars = searchedString:sub(delimiterIndex + 1, searchedClosedBracketIndex)
-
-				-- local repeatedChars = searchedString:sub(searchedDelimiterIndex, searchedClosedBracketIndex)
-				-- print('searchedDelimiterIndex: '..tostring(searchedDelimiterIndex))
-				-- print('searchedClosedBracketIndex: '..tostring(searchedClosedBracketIndex))
-				-- sleep(1)
 				
-				-- Add the repeated characters.
-				local repetition = tonumber(searchedString:sub(2, delimiterIndex - 1))
-
-				-- print('searchedString: '..tostring(searchedString))
-				-- print('openBracketIndex: '..tostring(openBracketIndex))
-				-- print('repetitionStringEndIndex: '..tostring(delimiterIndex - 1))
-				-- print('repetitionString: '..tostring(searchedString:sub(2, delimiterIndex - 1)))
-				-- print('repetition: '..tostring(repetition))
-				-- sleep(1)
-				-- print(type(searchedString))
-				-- print(#searchedString)
-				-- print(searchedString:find('['))
-				-- print(string.find('[288; ]', '['))
-				-- print(repeatedChars)
-				-- sleep(1)
-
-				unoptimizedString = unoptimizedString .. repeatedChars:rep(repetition)
+				if openBracketIndex == nil then
+					unoptimizedString = unoptimizedString .. char
+				end
 				
-				openBracketIndex = nil
+				if char == ']' then
+					-- More efficient to look at 'optimizedFrame' once like this, because we 'sub:' it twice later.
+					local closedBrackedIndex = currentIndex
+					local searchedString = optimizedFrame:sub(openBracketIndex, closedBrackedIndex)
+
+					local delimiterIndex
+					for i = 1, #searchedString do
+						local char = searchedString:sub(i, i)
+						if char == ';' then
+							delimiterIndex = i
+							break
+						end
+					end
+
+					local searchedDelimiterIndex = delimiterIndex + 2 - openBracketIndex
+					local searchedClosedBracketIndex = closedBrackedIndex - openBracketIndex
+					local repeatedChars = searchedString:sub(delimiterIndex + 1, searchedClosedBracketIndex)
+					
+					-- Add the repeated characters.
+					local repetition = tonumber(searchedString:sub(2, delimiterIndex - 1))
+
+					unoptimizedString = unoptimizedString .. repeatedChars:rep(repetition)
+					
+					openBracketIndex = nil
+				end
 			end
+
+			table.insert(self.unpackedOptimizedFrames, unoptimizedString)
+
+			cf.tryYield()
+		end
+	end,
+
+	getSelectedAnimationData = function(self, fileName)
+		if cfg.computerType ~= 'laptop' and cfg.computerType ~= 'desktop' then
+			error('You didn\'t enter a valid \'computerType\' name in the cfg file!')
 		end
 
-		table.insert(unpackedOptimizedFrames, unoptimizedString)
+		print('1/4 - Opening chosen data file...')
+
+		if not fileName then
+			error('There was an attempt to load a file name that doesn\'t exist; check if the chosen file name and the file name in the input folder match.')
+		end
+
+		local file = fs.open(cfg.computerType .. ' inputs/' .. fileName .. '.txt', 'r')
+
+		if not file then
+			error('There was an attempt to load a file name that doesn\'t exist; check if the chosen file name and the file name in the input folder match.')
+		end
 
 		cf.tryYield()
-    end
-end
 
-local function getSelectedAnimationData()
-	if cfg.computerType ~= 'laptop' and cfg.computerType ~= 'desktop' then
-		error('You didn\'t enter a valid \'computerType\' name in the cfg file!')
-	end
+		local startIndex = cf.find(fileName, '[') + 1
+		local endIndex = cf.find(fileName, ']') - 1
+		self.frameCount = tonumber(fileName:sub(startIndex, endIndex))
+		cf.tryYield()
 
-	print('1/4 - Opening chosen data file...')
-	local file = fs.open(cfg.computerType .. ' inputs/' .. cfg.fileName .. '.txt', 'r')
-	if not file then
-		error('There was an attempt to load a file name that doesn\'t exist; check if the chosen file name in the cfg file and the file name in the input folder match.')
-	end
-	cf.tryYield()
+		local cursorX, cursorY = term.getCursorPos()
+		local stringTab = {}
+		local i = 1
+		for lineStr in file.readLine do
+			table.insert(stringTab, lineStr)
 
-	local startIndex = cf.find(cfg.fileName, '[') + 1
-	local endIndex = cf.find(cfg.fileName, ']') - 1
-	frameCount = tonumber(cfg.fileName:sub(startIndex, endIndex))
-	cf.tryYield()
-
-	local cursorX, cursorY = term.getCursorPos()
-	local stringTab = {}
-	local i = 1
-	for lineStr in file.readLine do
-		table.insert(stringTab, lineStr)
-
-		if i % 100 == 0 or i == frameCount then
-			term.setCursorPos(cursorX, cursorY)
-			print('2/4 - Got '..tostring(i)..'/'..tostring(frameCount)..' data lines...')
-		end
-		i = i + 1
-
-		cf.yield()
-	end
-	
-	file:close()
-	cf.tryYield()
-
-	local finalLine = stringTab[#stringTab]
-	local compressedIndex = finalLine:find('compressed') + 11
-	local compressedIndexComma = finalLine:find(',', compressedIndex) - 1
-	compressed = finalLine:sub(compressedIndex, compressedIndexComma) == 'true'
-	cf.tryYield()
-	
-	-- This is necessary, as the first line is just blank line.
-	table.remove(stringTab, 1)
-	-- The general information data can be removed, so we're left with the frames.
-	table.remove(stringTab, #stringTab)
-
-	
-	local optimizedFrames = stringTab
-	cf.tryYield()
-	
-	if compressed then
-		unpackOptimizedFrames(optimizedFrames)
-	else
-		unpackedOptimizedFrames = optimizedFrames
-		-- print(unpackedOptimizedFrames[1])
-		-- print(unpackedOptimizedFrames[#unpackedOptimizedFrames - 30]:sub(1, 2000))
-		-- sleep(50)
-	end
-end
-
-local function createGeneratedCodeFolder()
-	if fs.exists('.generatedCodeFiles') then
-		local names = fs.list('.generatedCodeFiles')
-
-		for _, name in pairs(names) do
-			fs.delete('.generatedCodeFiles/'..tostring(name))
-		end
-	else
-		fs.makeDir('.generatedCodeFiles')
-	end
-end
-
-local function dataToGeneratedCode()
-	whileLoop = frameCount > 1 and cfg.loop
-	
-	local numberOfNeededFiles = math.ceil(frameCount / cfg.maxFramesPerGeneratedCodeFile)
-
-	local cursorX, cursorY = term.getCursorPos()
-	local i = 1
-
-	for generatedCodeFileIndex = 1, numberOfNeededFiles do
-		local handle = io.open('.generatedCodeFiles/'..generatedCodeFileIndex, 'w')
-
-		local frameOffset = (generatedCodeFileIndex - 1) * cfg.maxFramesPerGeneratedCodeFile
-
-		local frameCountToFile = math.min(frameCount - frameOffset, cfg.maxFramesPerGeneratedCodeFile)
-
-		local minFrames = frameOffset + 1
-		local maxFrames = frameOffset + frameCountToFile
-
-		for f = minFrames, maxFrames do
-			local string = '\ncf.frameWrite("' .. unpackedOptimizedFrames[f] .. '")'..
-			'\nos.queueEvent("r")'..
-			'\nos.pullEvent("r")'
-
-			if cfg.frameSleeping and cfg.frameSleep ~= -1 then
-				string = string..
-				'\nsleep(' .. tostring(cfg.frameSleep) .. ')'
-			end
-			
-			handle:write(string)
-			
-			if i % 100 == 0 or i == frameCount then
+			if i % 100 == 0 or i == self.frameCount then
 				term.setCursorPos(cursorX, cursorY)
-				print('3/4 - Generated '..tostring(i)..'/'..tostring(frameCount)..' frames...')
+				print('2/4 - Got '..tostring(i)..'/'..tostring(self.frameCount)..' data lines...')
 			end
 			i = i + 1
+
+			cf.yield()
+		end
+		
+		file:close()
+		cf.tryYield()
+
+		local finalLine = stringTab[#stringTab]
+		local compressedIndex = finalLine:find('compressed') + 11
+		local compressedIndexComma = finalLine:find(',', compressedIndex) - 1
+		self.compressed = finalLine:sub(compressedIndex, compressedIndexComma) == 'true'
+		cf.tryYield()
+
+		-- The general information data can be removed, so only the frames are left.
+		table.remove(stringTab, #stringTab)
+		local optimizedFrames = stringTab
+		cf.tryYield()
+		
+		if self.compressed then
+			self:unpackOptimizedFrames(optimizedFrames)
+		else
+			self.unpackedOptimizedFrames = optimizedFrames
+		end
+	end,
+
+	createGeneratedCodeFolder = function(self)
+		if fs.exists('.generatedCodeFiles') then
+			local names = fs.list('.generatedCodeFiles')
+
+			for _, name in pairs(names) do
+				fs.delete('.generatedCodeFiles/'..tostring(name))
+			end
+		else
+			fs.makeDir('.generatedCodeFiles')
+		end
+	end,
+
+	dataToGeneratedCode = function(self)
+		whileLoop = self.frameCount > 1 and cfg.loop
+		
+		local numberOfNeededFiles = math.ceil(self.frameCount / cfg.maxFramesPerGeneratedCodeFile)
+
+		local cursorX, cursorY = term.getCursorPos()
+		local i = 1
+
+		local framesToSleep = {}
+		for v = cfg.frameSleepSkipping, self.frameCount, cfg.frameSleepSkipping do
+			table.insert(framesToSleep, cf.round(v))
+		end
+		local frameSleepSkippingIndex = 1
+
+		for generatedCodeFileIndex = 1, numberOfNeededFiles do
+			local handle = io.open('.generatedCodeFiles/'..generatedCodeFileIndex, 'w')
+
+			local frameOffset = (generatedCodeFileIndex - 1) * cfg.maxFramesPerGeneratedCodeFile
+
+			local frameCountToFile = math.min(self.frameCount - frameOffset, cfg.maxFramesPerGeneratedCodeFile)
+
+			local minFrames = frameOffset + 1
+			local maxFrames = frameOffset + frameCountToFile
+
+			for f = minFrames, maxFrames do
+				local string = '\ncf.frameWrite("' .. self.unpackedOptimizedFrames[f] .. '")'..
+				'\nos.queueEvent("r")'..
+				'\nos.pullEvent("r")'
+
+				-- framesToSleep[frameSleepSkippingIndex] might cause errors when trying to access stuff outside of the table's scope
+				if cfg.frameSleeping and cfg.frameSleep ~= -1 and f == framesToSleep[frameSleepSkippingIndex] then
+					string = string..
+					'\nsleep(' .. tostring(cfg.frameSleep) .. ')'
+					frameSleepSkippingIndex = frameSleepSkippingIndex + 1
+				end
+				
+				handle:write(string)
+				
+				if i % 100 == 0 or i == self.frameCount then
+					term.setCursorPos(cursorX, cursorY)
+					print('3/4 - Generated '..tostring(i)..'/'..tostring(self.frameCount)..' frames...')
+				end
+				i = i + 1
+				
+				cf.tryYield()
+			end
+			
+			handle:close()
 			
 			cf.tryYield()
 		end
-		
-		handle:close()
-		
-		cf.tryYield()
-	end
-end
+	end,
 
-local function executeRegularCode()
-	for f = 1, frameCount do
-		term.setCursorPos(1,1)
-		write(unpackedOptimizedFrames[f])
-		if cfg.frameSleeping and cfg.frameSleep ~= -1 then
-			sleep(cfg.frameSleep)
-		end
-		cf.tryYield()
-	end
-end
+	-- CODE EXECUTION --------------------------------------------------------
 
--- CODE EXECUTION --------------------------------------------------------
-
-local function setup()
-    importConfig()
-	if not rs.getInput(cfg.leverSide) then
-		importAPIs()
-		-- importAnimation() -- Request animation using http calls.
-        getSelectedAnimationData()
+	loadAnimation = function(self, fileName)
+		self:importAPIs()
+		-- self:importAnimationData() -- Request an animation using a http call.
+		self:getSelectedAnimationData(fileName)
 		cf.tryYield()
 
 		if cfg.generateOptimizedCode then
-			createGeneratedCodeFolder()
-			dataToGeneratedCode()
+			self:createGeneratedCodeFolder()
+			self:dataToGeneratedCode()
 		end
-    end
-end
+		cf.tryYield()
+	end,
 
-local function main()
-	if not rs.getInput(cfg.leverSide) then
-		if cfg.generateOptimizedCode then
-			print('4/4 - Executing code...')
-
-			local len = #fs.list('.generatedCodeFiles')
-			
-			-- local handles = {}
-			-- for i = 1, len do
-			-- 	local handle = io.open('.generatedCodeFiles/'..i, 'r')
-			-- 	table.insert(handles, handle)
-			-- end
-
-			cf.yield()
-			while true do
-				if not rs.getInput(cfg.leverSide) then
-					for i = 1, len do
-						-- local handle = handles[i]
-
-						-- What goes here?
-
-						shell.run('.generatedCodeFiles/'..tostring(i))
-					end
-				else
-					sleep(1)
+	_playAnimation = function(self, len)
+		if cfg.playAnimationBool then
+			for i = 1, len do
+				if cfg.playAnimationBool then
+					shell.run('.generatedCodeFiles/'..tostring(i))
 				end
-				cf.tryYield()
 			end
 		else
-			print('Executing regular code...')
-
-			whileLoop = frameCount > 1 and cfg.loop
-			if whileLoop then
-				while true do
-					executeRegularCode()
-				end
-			else
-				executeRegularCode()
-			end
+			sleep(1)
 		end
-        term.setCursorPos(width, height)
+		cf.tryYield()
+	end,
+
+	playAnimation = function(self, loop)
+		local len = #fs.list('.generatedCodeFiles')
+
+		if loop then
+			while true do
+				self:_playAnimation(len)
+			end
+		else
+			self:_playAnimation(len)
+		end
 	end
+
+}
+
+local animation = Animation:new()
+animation:importConfig()
+if not rs.getInput(cfg.leverSide) then
+	animation:loadAnimation(cfg.fileName)
+	print('4/4 - Executing code...')
+	animation:playAnimation(false)
+	local width, height = term.getSize()
+	term.setCursorPos(width - 1, height)
 end
 
-setup()
-main()
