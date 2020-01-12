f432b01b603151e34bef42a9274e26f8e8ffb82b
MyNameIsTrez <32989873+MyNameIsTrez@users.noreply.github.com>
2020-01-12 02:48:21 +0100

    Animation program now done!

--- Random Projects/computer/19/main.lua
+++ Random Projects/computer/19/main.lua
@@ -3,9 +3,9 @@
 function importAPIs()
 	local APIs = {
 		{id = 'p9tSSWcB', name = 'cf'}, -- Common Functions.
-		{id = 'LNab4wrv', name = 'an'}, -- Animation.
         {id = "4nRg9CHU", name = "json"}, -- HTTPS needs JSON.
-        {id = "iyBc3BWj", name = "https"}, -- HTTPS.
+        {id = "iyBc3BWj", name = "https"}, -- Animation needs HTTPS.
+		{id = 'LNab4wrv', name = 'an'}, -- Animation.
 	}
 
 	fs.delete('apis') -- Deletes the folder, with every API file in it.
@@ -28,13 +28,12 @@ term.setCursorPos(1, 1)
 
 if not rs.getInput(cfg.leverSide) then
     -- Setup.
-    local animation = an.Animation:new(shell)
+	local animation = an.Animation:new(shell)
     animation:setShell(shell)
-    animation:importConfig()
 
-    -- Main.
-	animation:loadAnimation(cfg.fileName)
-	print('4/4 - Executing code...')
+	-- Main.
+	local outputFolder = 'Animations/size_' .. cfg.animationSize.width .. 'x' .. cfg.animationSize.height
+	animation:loadAnimation(cfg.fileName, cfg.animationSize, outputFolder)
 	animation:playAnimation(cfg.loop)
 	local width, height = term.getSize()
 	term.setCursorPos(width - 1, height)
