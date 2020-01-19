304dd76cb2d0d4e50304afdc8df7fe9958b19a97
MyNameIsTrez <32989873+MyNameIsTrez@users.noreply.github.com>
2020-01-19 03:22:43 +0100

    threedee

--- Random Projects/computer/25/main.lua
+++ Random Projects/computer/25/main.lua
@@ -29,30 +29,48 @@ importConfig()
 if not rs.getInput(cfg.leverSide) then
 	-- Setup.
 	local cubes = {
-		{
-			x = 0,
-			y = 0,
-			z = 0,
-			scale = 1,
-		},
+		{0,0,0},
+		-- {2,2,2},
 	}
 
-	width, height = term.getSize()
-    width = width - 1
-    framebuffer = fb.FrameBuffer:new(width, height)
-    framebuffer:createBuffer()
-    threedee = td.ThreeDee:new(framebuffer, cubes, width, height, 1)
+	-- local cubes = {}
+	-- for i = 1, 5 do
+	-- 	for j = 1, 5 do
+	-- 		cubes[#cubes + 1] = {j, i, 0}
+	-- 	end
+	-- end
 
-    -- Main.
-    threedee:draw()
+	local offsets = { 7, -10 }
+
+	local width, height = term.getSize()
+	width = width - 1
+
+	local blockDiameter = 10
+	local connectionChar = '@'
+	local pointChar = '+'
+	
+    local framebuffer = fb.FrameBuffer.new(width, height)
+    local threedee = td.ThreeDee.new(framebuffer, width, height, cubes, blockDiameter, offsets, connectionChar, pointChar)
+
+	-- Main.
+	-- cf.printTable(threedee.cubesCorners)
+
+	-- threedee:line(0, 0, 20, 20, '8')
+
+	-- threedee:drawConnections()
+	threedee:drawFill()
+	-- threedee:fill(threedee.cubesCorners[1][1][1], threedee.cubesCorners[1][1][2], threedee.cubesCorners[1][2][1], threedee.cubesCorners[1][2][2], threedee.cubesCorners[1][4][1], threedee.cubesCorners[1][4][2])
+	-- threedee:fill(threedee.cubesCorners[1][1], threedee.cubesCorners[1][2], threedee.cubesCorners[1][5])
     framebuffer:draw()
 
     while true do
         local event, keyNum = os.pullEvent()
         if (event == "key") then
             local char = keys.getName(keyNum)
-            threedee:moveCamera(char)
-            threedee:draw()
+			threedee:moveCamera(char)
+			threedee:getCubesCorners(threedee.cubesCoords, threedee.blockDiameter)
+			-- threedee:drawConnections()
+            threedee:drawFill()
             framebuffer:draw()
         end
     end
