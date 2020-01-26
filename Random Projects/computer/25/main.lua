8257d6a4beb481e7388f54503346175be9045b77
MyNameIsTrez <32989873+MyNameIsTrez@users.noreply.github.com>
2020-01-26 18:46:12 +0100

    Finished cube code, now moving it into the threedee API.

--- Random Projects/computer/25/main.lua
+++ Random Projects/computer/25/main.lua
@@ -33,6 +33,9 @@ if not rs.getInput(cfg.leverSide) then
 	angle = 0
 
 	while true do
+		-- Code from this tutorial:
+		-- https://youtu.be/p4Iz0XJY-Qk
+
 		local rotationX = {
 			{ 1, 0, 0 },
 			{ 0, math.cos(angle), -math.sin(angle) },
@@ -49,11 +52,6 @@ if not rs.getInput(cfg.leverSide) then
 			{ math.cos(angle), -math.sin(angle), 0 },
 			{ math.sin(angle),  math.cos(angle), 0 },
 			{ 0, 0, 1 }
-		}
-
-		local projection = {
-			{1, 0, 0},
-			{0, 1, 0}
 		}
 
 		local points = {}
@@ -75,20 +73,22 @@ if not rs.getInput(cfg.leverSide) then
 
 		for i = 1, #points do
 			local v = points[i]
-			local m = matrix.vecToMat(v:mul(100)) -- Might need to mul after projection instead.
+			local m = matrix.vecToMat(v)
 			local rotated = matrix.matMul(rotationX, m)
 			rotated = matrix.matMul(rotationY, rotated)
 			rotated = matrix.matMul(rotationZ, rotated)
-			local projected2d = matrix.matMul(projection, rotated)
-
-			-- ideally
-			-- local x, y = centerX + projected2d.x * 1.5, centerY + projected2d.y
 			
-			-- local _x, _y = projected2d[1][1], projected2d[2][1]
-			-- local x, y = centerX + _x * 1.5, centerY + _y
-
+			local distance = 2
+			local z = 1 / (distance - rotated[3][1])
+			local projection = {
+				{z, 0, 0},
+				{0, z, 0}
+			}
+			
+			local projected2d = matrix.matMul(projection, rotated)
+			projected2d[1][1] = projected2d[1][1] * 100
+			projected2d[2][1] = projected2d[2][1] * 100
 			projected[i] = projected2d
-			-- framebuffer:writeChar(x, y, 'p')
 		end
 
 		function connect(i, j)
@@ -100,18 +100,26 @@ if not rs.getInput(cfg.leverSide) then
 			local _x2, _y2 = b[1][1], b[2][1]
 			local x2, y2 = centerX + _x2 * 1.5, centerY + _y2
 
-			framebuffer:writeLine(x1, y1, x2, y2, 'c')
+			framebuffer:writeLine(x1, y1, x2, y2, 'l')
 		end
 
-		connect(1, 2)
+		-- Draw lines between corners.
+		for i = 1, 4 do
+			connect(i, i % 4 + 1) -- Front.
+			connect(i, i + 4) -- Middle.
+			connect(i + 4, i % 4 + 5) -- Back.
+		end
 
-		-- for i = 1, 12 do
-
+		-- -- Draw corners.
+		-- for _, v in ipairs(projected) do
+		-- 	local _x, _y = v[1][1], v[2][1]
+		-- 	local x, y = centerX + _x * 1.5, centerY + _y
+		-- 	framebuffer:writeChar(x, y, 'c')
 		-- end
 
 		framebuffer:draw()
 
-		angle = angle + 0.01
+		angle = angle + 0.025
 
 		os.queueEvent('yield')
 		os.pullEvent('yield')
