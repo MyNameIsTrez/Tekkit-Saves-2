08c1bb3a2103bc48e7712b25d25442e280f0220f
MyNameIsTrez <32989873+MyNameIsTrez@users.noreply.github.com>
2020-03-30 15:55:38 +0200

    Added code for drawing the OS.

--- ComputerCraft/computer/0/BackwardsOS/programs/Animation/Animation.lua
+++ ComputerCraft/computer/0/BackwardsOS/programs/Animation/Animation.lua
@@ -28,7 +28,7 @@
 
 -- CREATING AN ANIMATION OBJECT ---------------------------------
 
-local settings2 = {
+local settings = {
 	shell = shell,
 	frameSleeping = cfg.frameSleeping,
 	frameSleep = cfg.frameSleep,
@@ -39,44 +39,128 @@ local settings2 = {
 	progressBool = cfg.progressBool,
 	useMonitor = cfg.useMonitor,
 	loop = cfg.progressBool,
+	offset = cfg.offset,
 	folder = cfg.folder,
-	animationSize = { width = 8, height = 8 },
+	animationSize = cfg.animationSize,
 }
 
-local animation2 = an.Animation:new(settings2)
+local animation = an.Animation:new(settings)
 
-animation2.offset = { x = 50, y = 50 }
+animation:setCharCodeOffset(4, 0)
 
-animation2:writeCharCode(72)  -- 'H'
-animation2:writeCharCode(101) -- 'e'
-animation2:writeCharCode(108) -- 'l'
-animation2:writeCharCode(108) -- 'l'
-animation2:writeCharCode(111) -- 'o'
-animation2:writeCharCode(32)  -- ' '
-animation2:writeCharCode(87)  -- 'W'
-animation2:writeCharCode(111) -- 'o'
-animation2:writeCharCode(114) -- 'r'
-animation2:writeCharCode(108) -- 'l'
-animation2:writeCharCode(100) -- 'd'
-animation2:writeCharCode(33)  -- '!'
 
-animation2.offset = { x = 154, y = 50 }
-animation2.fileName = 'caramelldansen_undertale'
+animation:writeCharCode(179) -- 'line vertical'
 
-animation2.animationSize = { width = 227, height = 85 }
-animation2.loop = true
-animation2.frameSleepSkipping = 1
-animation2.frameSleep = 0.15
+animation:writeCharCode(195, 0, 1) -- 'line vertical right'
 
-animation2:loadAnimation()
-animation2:playAnimation()
+animation:writeCharCode(179, 0, 1) -- 'line vertical'
+
+animation:writeCharCode(180, 0, 1) -- 'line vertical left'
+
+animation:setCharCodeOffset(5, 3)
+
+for i = 1, 16 do -- 16
+	animation:writeCharCode(179, 0, 1) -- 'line vertical'
+end
+
+animation:setCharCodeOffset(5, 1)
+
+for i = 1, 39 do
+	animation:writeCharCode(196) -- 'line horizontal'
+end
+
+animation:writeCharCode(193) -- 'line horizontal up'
+
+animation:writeCharCode(196) -- 'line horizontal'
+
+animation:writeCharCode(193) -- 'line horizontal up'
+
+for i = 1, 6 do -- 6th only draws one character on-screen
+	animation:writeCharCode(196) -- 'line horizontal'
+end
+
+animation:setCharCodeOffset(44, 0)
+
+animation:writeCharCode(179) -- 'line vertical'
+
+animation:addCharCodeOffset(1, 0)
+if http then
+	animation.fileName = 'internet_icon'
+	animation:loadAnimation()
+	animation:playAnimation()
+else
+	animation:writeCharCode(33, 0, 0) -- '!'
+end
+
+animation:writeCharCode(179) -- 'line vertical'
+
+animation:writeCharCode(49) -- '1'
+animation:writeCharCode(51) -- '3'
+animation:writeCharCode(58) -- ':'
+animation:writeCharCode(51) -- '3'
+animation:writeCharCode(55) -- '7'
+
+animation:setCharCodeOffset(5, 3)
+
+for i = 1, 5 do
+	animation:writeCharCode(196, -1) -- 'line horizontal'
+end
+
+animation:setCharCodeOffset(-1, 0)
+animation:writeCharCode(62)  -- '>'
+animation:writeCharCode(80)  -- 'P'
+animation:writeCharCode(82)  -- 'R'
+animation:writeCharCode(79)  -- 'O'
+animation:writeCharCode(71)  -- 'G'
+
+animation:setCharCodeOffset(0, 1)
+animation:writeCharCode(80)  -- 'P'
+animation:writeCharCode(82)  -- 'R'
+animation:writeCharCode(79)  -- 'O'
+animation:writeCharCode(71)  -- 'G'
+
+animation:setCharCodeOffset(0, 2)
+animation:writeCharCode(78)  -- 'N'
+animation:writeCharCode(69)  -- 'E'
+animation:writeCharCode(87)  -- 'W'
+
+animation:setCharCodeOffset(5, 0)
+animation:addOffset(2, 2)
+
+animation:writeCharCode(72)  -- 'H'
+animation:writeCharCode(101) -- 'e'
+animation:writeCharCode(108) -- 'l'
+animation:writeCharCode(108) -- 'l'
+animation:writeCharCode(111) -- 'o'
+animation:writeCharCode(32)  -- ' '
+animation:writeCharCode(87)  -- 'W'
+animation:writeCharCode(111) -- 'o'
+animation:writeCharCode(114) -- 'r'
+animation:writeCharCode(108) -- 'l'
+animation:writeCharCode(100) -- 'd'
+animation:writeCharCode(33)  -- '!'
+
+animation:setOffset(125, 38)
+animation.fileName = 'caramelldansen_undertale'
+
+animation.animationSize = { width = 227, height = 85 }
+-- animation.loop = true
+animation.frameSleepSkipping = 1
+animation.frameSleep = 0.15
+
+animation:loadAnimation()
+animation:playAnimation()
+
+term.setCursorPos(1, 31)
+local time = wt.getTime('Europe', 'Amsterdam')
+cf.printTable(time)
 
 -- DRAWING ANIMATIONS ENDED ----------------------------------------------
 
 -- Hide the cursor by placing it at the bottom-right.
-local width, height = term.getSize()
-term.setCursorPos(width - 1, height)
+-- local width, height = term.getSize()
+-- term.setCursorPos(width - 1, height)
 
-if useMonitor and monitorSide then
-	term.restore()
-end
+-- if cfg.useMonitor and cfg.monitorSide then
+-- 	term.restore()
+-- end
